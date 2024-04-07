0c06f303a14e900095133c697f3021597ded66c4
itadminss <98080070+itadminss@users.noreply.github.com>
2024-04-07 16:14:25 +0700

    Week02

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Welcome from './screens/Welcome';
+import Ex01 from './screens/week2/Ex01';
+import Ex02 from './screens/week2/Ex02';
+import Ex03 from './screens/week2/Ex03';
+import Ex04 from './screens/week2/Ex04';
+import Ex05 from './screens/week2/Ex05';
+import Ex06 from './screens/week2/Ex06';
+import Ex07 from './screens/week2/Ex07';
+import Ex08 from './screens/week2/Ex08';
+import Ex10 from './screens/week2/Ex10';
+import Ex11 from './screens/week2/Ex11';
+import Ex12 from './screens/week2/Ex12';
+import Ex09 from './screens/week2/Ex09';
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <Welcome/>
+    <Ex09 />
   );
 }
 
